3fc940f7e0abe242b7c663f4711d9fe85594d1e5
mohak1827 <mohak1827vk@gmail.com>
2025-11-26 04:23:36 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -40,3 +40,7 @@ app.use('/api/favorites', require('./routes/favoritesRoutes')); // Favorites rou
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("Artelier Backend is running 🚀");
+});
