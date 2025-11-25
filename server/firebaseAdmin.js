ae1ff8eca1c5384da7f01ebcb71d7bd79681b0d9
mohak1827 <mohak1827vk@gmail.com>
2025-11-26 03:24:04 +0530

    Fix backend package.json for Render deployment

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,5 +1,11 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccount.json');
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
